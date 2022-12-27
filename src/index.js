18efec2a24f13082eab3d372e3b6420c83af99b0
Rahul <rahulrdx1308@gmail.com>
2022-12-27 14:19:23 +0530

    register login using jwt hashing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const connect = require("./configs/db");
 const gameController = require("./controllers/game.controller");
 const cartController = require("./controllers/cart.controller");
+const { register, login } = require("./controllers/auth.controller");
+const userController = require("./controllers/user.controller");
 
 const app = express();
 const dotenv = require("dotenv");
@@ -13,6 +15,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/game", gameController);
 app.use("/cart", cartController);
+ app.use("/users", userController);
+
+app.post("/register", register);
+
+app.post("/login", login);
 
 
 const port = process.env.PORT || 5000;
